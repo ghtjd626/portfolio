d54fc9b029f1681a4e90b124d0806243e805b0bc
KeunHo <ghtjd626@naver.com>
2021-02-05 16:53:09 +0900

    no message

--- portfolio/src/component/header/index.js
+++ portfolio/src/component/header/index.js
@@ -1,17 +1,11 @@
 import styles from './header.module.css';
 
 const Header = () => {
-  const scroll = () => {
-    window.scrollBy({ top: 801, left: 0, behavior: 'smooth' });
-  };
-
   return (
     <>
       <nav className={styles.headerNav}>
         <a href="#home">HOME</a>
-        <a href="#about" onClick={scroll}>
-          ABOUT
-        </a>
+        <a href="#about">ABOUT</a>
         <a href="/">PORTFOLIO</a>
         <a href="/">BLOG</a>
         <a href="/">CONTACT</a>
